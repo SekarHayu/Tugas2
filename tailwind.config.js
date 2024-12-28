447921cee45cbdb4684f6baf072d3e0350d89804
SekarHayu <16sekarhayu@gmail.com>
2024-12-28 18:30:11 +0700

    Menambahkan file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/smti.jpg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
